bd684ec3f38029342461a97a1353de82fa4e78ec
Mauricio Matamoros <kyordhel@gmail.com>
2024-12-24 17:24:51 -0600

    Adds clipscontrol beta

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ fi
 #Script to start CLIPS
 xterm -hold -e "cd build/clipsserver && ./clipsserver" &
 sleep 0.5
+xterm -hold -e "cd build/clipscontrol && ./clipscontrol" &
 
-# xterm -hold -e "cd build/clipscontrol && ./clipscontrol" &
 # xterm -hold -e "cd tests/pytest && ./test.py"
 xterm -hold -e "cd build/tests && ./testclient"
