14c46bcf34991537eacf26b9ec6c4ec3dc08db70
Mauricio Matamoros <kyordhel@gmail.com>
2025-01-28 22:55:07 -0600

    Remove unnecessary connection messages

--- clipsserver/src/server.cpp
+++ clipsserver/src/server.cpp
@@ -131,11 +131,6 @@ void Server::acceptHandler(const boost::system::error_code& error, std::shared_p
 		publishStatus();
 	}
 
-	printf("Connected clients:\n");
-	for(auto it = clients.begin(); it != clients.end(); ++it)
-	printf("\t%s, %s\n", it->first.c_str(), it->second->getEndPointStr().c_str());
-
-
 	std::shared_ptr<tcp::socket> nextSckt(new tcp::socket(io_context));
 	acceptorPtr->async_accept(
 		*nextSckt,
