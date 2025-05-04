656c85ef44a2fd50de5c86d9b441bdee20fb72d8
Mauricio Matamoros <kyordhel@gmail.com>
2025-05-03 21:56:21 -0600

    Add start_planning.sh

---
+++ start_planning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f ./build/clipsserver/clipsserver ]; then
+	if [ ! -d ./build ]; then
+		mkdir -p build;
+	fi
+	cd build;
+	cmake .. && make;
+	cd ..
+fi
+
+
+
+#Script to start CLIPS
+xterm -geometry 80x27+100+100 -hold -e "cd build/clipsserver && ./clipsserver" &
+sleep 0.3
+xterm -geometry 160x27+100+480 -hold -e "cd /build/clipscontrol && ./clipscontrol" &
+sleep 0.1
+
